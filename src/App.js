f23aaac0ec02451f60699042624acc22cb71f03f
Youngmin <gosmain@naver.com>
2022-03-09 23:59:22 +0900

    front setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import { useReactiveVar } from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <div className="App">
-     
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+    <GlobalStyles />
+    <Router>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+    </ThemeProvider>
   );
 }
 
