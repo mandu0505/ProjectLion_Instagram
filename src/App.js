8d477e231cb1d65155dad84c802765d561a60db4
Youngmin <gosmain@naver.com>
2022-03-10 18:23:11 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import { useReactiveVar } from "@apollo/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./screens/Home";
-import Login from "./screens/Login";
-import NotFound from "./screens/NotFound";
-import { darkModeVar, isLoggedInVar } from "./apollo";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, GlobalStyles, lightTheme } from "./styles";
+import Header from "./components/Header";
 
 function App() {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
-  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-    <GlobalStyles />
-    <Router>
-      <Routes>
-        <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-    </ThemeProvider>
+    <Header />
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+import { AiFillHome, AiOutlineHeart } from "react-icons/ai";
+import { BiPaperPlane, BiSearchAlt2 } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { FaRegCompass } from "react-icons/fa";
+import { FiPlusSquare } from "react-icons/fi";
+import logo from "../components/logo.png";
+
+const SHeader = styled.header`
+  width: 100%;
+  border-bottom: 1px solid #efefef;
+  padding: 18px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  max-width: 930px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Column = styled.div`
+  display: flex;
+`;
+const Img = styled.img`
+  width: 100px;
+`;
+const Label = styled.div`
+  left: 70px;
+  width: 200px;
+  padding: 7px 10px;
+  border-radius: 5px;
+  background-color: #efefef;
+  border: none;
+  position: relative;
+  color: gray;
+  .icon {
+    margin-bottom: -4px;
+  }
+`;
+const Input = styled.input`
+  width: 90%;
+  height: 100%;
+  background-color: #efefef;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`;
+const Icon = styled.span`
+  margin-left: 15px;
+`;
+const IconsContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+function Header() {
+  return (
+    <SHeader>
+      <Wrapper>
+        <Column>
+          <Img alt="logo" type="image/png" src={logo} />
+        </Column>
+        <Column>
+          <Label>
+            <BiSearchAlt2 className="icon" />
+            <Input type="text" placeholder="검색" />
+          </Label>
+        </Column>
+        <Column>
+          <IconsContainer>
+            <Icon>
+              <AiFillHome />
+            </Icon>
+            <Icon>
+              <BiPaperPlane />
+            </Icon>
+            <Icon>
+              <FiPlusSquare />
+            </Icon>
+            <Icon>
+              <FaRegCompass />
+            </Icon>
+            <Icon>
+              <AiOutlineHeart />
+            </Icon>
+            <Icon>
+              <CgProfile />
+            </Icon>
+          </IconsContainer>
+        </Column>
+      </Wrapper>
+    </SHeader>
+  );
+}
+export default Header;
