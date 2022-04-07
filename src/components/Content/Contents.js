8abd839c6e564c3e7192ec480d01120896bf66c8
Youngmin <gosmain@naver.com>
2022-04-07 21:49:02 +0900

    homepage body contents & profile

--- src/components/Content/Contents.js
+++ src/components/Content/Contents.js
@@ -1,17 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+import { FaRegUserCircle } from "react-icons/fa";
+
+const Head = styled.div`
+  margin-left: 250px;
+  .top {
+    display: flex;
+    .logo {
+      font-size: 30px;
+    }
+    .title {
+      margin-top: 5px;
+      margin-left: 50px;
+    }
+    .dots {
+      margin-left: 450px;
+      margin-top: 5px ;
+    }
+  }
+  .photo {
+    text-align: center;
+    margin-top: 0px;
+    font-size: 50px;
+    font-weight: 800;
+    background-color: #000;
+    color: #fff;
+    width: 52%;
+    height: 400px;
+  }
+`;
 
 function Contents() {
   return (
-    <>
-      <div>
+    <Head>
+      <div className="top">
         <div>
-          <div>프사</div>
-          <div>사람</div>
+          <FaRegUserCircle className="logo" />
         </div>
-        <div>쩜쩜쩜</div>
+        <div className="title">사람</div>
+        <div className="dots">쩜쩜쩜</div>
       </div>
-      <div>사진</div>
-    </>
+      <div className="photo">사진</div>
+    </Head>
   );
 }
 
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -3,12 +3,57 @@ import { useState } from "react";
 import styled from "styled-components";
 import Header from "../components/Navbar";
 import Popup from "../components/Navbar/Popup";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FiSettings } from "react-icons/fi";
 
 const Head = styled.div`
   &.active {
     opacity: 0.1;
     pointer-events: none;
     user-select: none;
+  }
+  .top {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin-left: 250px;
+    .photo {
+      margin-top: 100px;
+      margin-left: 100px;
+      font-size: 150px;
+    }
+    .contents {
+      margin-top: 100px;
+      margin-left: -100px;
+      .name {
+        font-size: 25px;
+        margin-right: 100px;
+      }
+      .profile {
+        font-weight: 600;
+        border: 1px solid gray;
+        padding: 3px 5px 3px 5px;
+        border-radius: 5px;
+        margin-top: -30px;
+      }
+      .setting {
+        margin-left: 30px;
+        font-size: 20px;
+        margin-top: 30px;
+      }
+      .numbers {
+        margin-top: 50px;
+        .art1 {
+        }
+        .follower {
+          margin-left: 30px;
+        }
+        .follow {
+          margin-left: 30px;
+        }
+      }
+    }
+  }
+  .bottom {
   }
 `;
 
@@ -23,7 +68,26 @@ function Profile() {
     <>
       <Head className={blur}>
         <Header toggle={toggle} />
-      <h1>프로필</h1>
+        <div className="top">
+          <div className="photo">
+            <FaRegUserCircle />
+          </div>
+          <div className="contents">
+            <div>
+              <span className="name">이름</span>
+              <span className="profile">프로필 편집</span>
+              <span className="setting">
+                <FiSettings />
+              </span>
+            </div>
+            <div className="numbers">
+              <span className="art1">게시물 0</span>
+              <span className="follower">팔로워 100</span>
+              <span className="follow">팔로우 100</span>
+            </div>
+          </div>
+        </div>
+        <div className="bottom"></div>
       </Head>
       <Popup toggle={toggle} popup={popup} />
     </>
